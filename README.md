aff92109de4bf98cd133fa4e0e7f44b139a52f58
root <root@MSI.localdomain>
2018-11-02 19:51:17 -0700

    added function for drawing operations between vectors (both to experiments and javascript folder)

---
+++ README.md
@@ -0,0 +1,29 @@
+### Neural Network Weight Debugger (nn-wd) Visualization Tool
+Tool to debug neural networks, specifically as their weights flow through the computational graph.
+
+#### Sketch
+<img src="sketch.jpg"/>
+
+### Development
+Requirements:
+
+    # Python 3
+    python3 -m venv p3
+    source ./p3/bin/activate
+    # Libraries (pip based)
+    pip install numpy
+    pip install tensorflow==1.9.0
+    # Libraries (custom)
+    curl -LO https://github.com/sawatzkylindsey/pytils/archive/master.zip
+    cd pytils-master/
+    make install
+
+Running:
+
+    python dev-server.py -v
+
+Running after system restart:
+    
+    python3 -m venv p3
+    source ./p3/bin/activate
+    python dev-server.py -v
--- experiments/drawing.js
+++ experiments/drawing.js
@@ -10,7 +10,13 @@ $(document).ready(function () {
 
     drawWeightsForVector(top, left, width, heigth, min, max, vector);
 
-    drawWeightsForVector(top+30, left+400, width/2.0, heigth, min, max, vector);
+    drawWeightsForVector(top + 30, left + 400, width / 2.0, heigth, min, max, vector);
+
+    drawOperationSign("dotProduct", 320, 100, 80, '#abc');
+
+    drawOperationSign("addition", 320, 400, 40, '#abc');
+
+    drawOperationSign("equals", 320, 700, 200, '#abc');
 
 });
 
@@ -68,3 +74,89 @@ function drawWeightsForVector(top, left, width, height, min, max, vector) {
         .attr("stroke-width", strokeWidth)
         .attr("fill", "none");
 }
+
+
+function drawOperationSign(operation, top, left, size, color) {
+
+    var strokeWidth = 2;
+
+    var operationData = [{
+        "center": 0.5 * size,
+        "elementRadius": 0.4 * size,
+
+        "dotProductRadius": 0.125 * size,
+
+        "plusRectLongSide": 0.6 * size,
+        "plusRectShortSide": 0.08 * size,
+
+        "equalsRectLongSide": 0.5 * size,
+        "equalsRectShortSide": 0.08 * size
+    }];
+
+    var svgContainer = d3.select("body").append("svg")
+        .style('position', 'absolute')
+        .style('top', top)
+        .style('left', left)
+        .style('width', size)
+        .style('height', size);
+
+    var operator = svgContainer.selectAll("g")
+        .data(operationData)
+        .enter()
+        .append("g");
+    operator //background
+        .append("circle")
+        .attr("cx", function (d) { return d.center; })
+        .attr("cy", function (d) { return d.center; })
+        .attr("r", function (d) { return d.elementRadius; })
+        .style("fill", function () { return color; })
+        .attr("stroke-width", strokeWidth)
+        .attr("stroke", "black");
+
+
+    switch (operation) {
+        case "dotProduct":
+            operator //dot product
+                .append("circle")
+                .attr("cx", function (d) { return d.center; })
+                .attr("cy", function (d) { return d.center; })
+                .attr("r", function (d) { return d.dotProductRadius; })
+                .style("fill", function () { return "black"; });
+            break;
+        case "addition":
+            operator //horizontal plus rect
+                .append("rect")
+                .attr("x", function (d) { return d.center - d.plusRectShortSide * 3.75; })
+                .attr("y", function (d) { return d.center - d.plusRectShortSide * 0.5; })
+                .attr("width", function (d) { return d.plusRectLongSide; })
+                .attr("height", function (d) { return d.plusRectShortSide; })
+                .style("fill", function () { return "black"; });
+            operator //vertical plus rect
+                .append("rect")
+                .attr("y", function (d) { return d.center - d.plusRectShortSide * 3.75; })
+                .attr("x", function (d) { return d.center - d.plusRectShortSide * 0.5; })
+                .attr("height", function (d) { return d.plusRectLongSide; })
+                .attr("width", function (d) { return d.plusRectShortSide; })
+                .style("fill", function () { return "black"; });
+            break;
+        case "equals":
+            operator //equals upper rect
+                .append("rect")
+                .attr("x", function (d) { return d.center - d.equalsRectShortSide * 3.2; })
+                .attr("y", function (d) { return d.center - d.equalsRectShortSide * 1.5; })
+                .attr("width", function (d) { return d.equalsRectLongSide; })
+                .attr("height", function (d) { return d.equalsRectShortSide; })
+                .style("fill", function () { return "black"; });
+            operator //equals lower rect
+                .append("rect")
+                .attr("x", function (d) { return d.center - d.equalsRectShortSide * 3.2; })
+                .attr("y", function (d) { return d.center + d.equalsRectShortSide; })
+                .attr("width", function (d) { return d.equalsRectLongSide; })
+                .attr("height", function (d) { return d.equalsRectShortSide; })
+                .style("fill", function () { return "black"; });
+            break;
+        default:
+            break;
+    }
+}
+
