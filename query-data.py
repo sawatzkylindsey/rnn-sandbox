94b570286ab675db00dac9508703d2c79ace7011
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2019-07-04 15:53:02 -0700

    Quick implementation of relative matching.

--- query-data.py
+++ query-data.py
@@ -28,13 +28,19 @@ def main(argv):
     ap = ArgumentParser(prog="query-data")
     ap.add_argument("-v", "--verbose", default=False, action="store_true", help="Turn on verbose logging.")
     ap.add_argument("--limit", type=int, default=10)
-    ap.add_argument("--match", choices=["include", "sequence"], default="include")
+    ap.add_argument("--match", choices=["include", "sequence", "relative"], default="include")
     ap.add_argument("data_dir")
     ap.add_argument("kind", choices=["train", "test"])
     ap.add_argument("words", nargs="*", default=None)
     aargs = ap.parse_args(argv)
     setup_logging(".%s.log" % os.path.splitext(os.path.basename(__file__))[0], aargs.verbose, False, True, True)
     logging.debug(aargs)
+
+    if aargs.match == "relative":
+        # Quickest way to implement relative is just to make it correct for N = 2.
+        assert len(aargs.words) == 2
+
+    truncated = False
     count = 0
 
     for xy in data.stream_data(aargs.data_dir, aargs.kind):
@@ -43,13 +49,14 @@ def main(argv):
 
         if matches(sequence, aargs.words, aargs.match):
             count += 1
-            user_log.info("Instance: %s" % " ".join(sequence))
+            logging.debug("Instance: %s" % " ".join(sequence))
 
         if count >= aargs.limit:
-            user_log.info("Truncating..")
+            logging.debug("Truncating..")
+            truncated = True
             break
 
-    user_log.info("Found %d instances." % count)
+    user_log.info("Found %d%s instances." % (count, " (truncated)" if truncated else ""))
     return 0
 
 
@@ -59,7 +66,7 @@ def matches(sequence, words, match):
 
     if match == "include":
         return all([include in sequence for include in words])
-    else:
+    elif match == "sequence":
         # To match a sequence, certainly all the words must be included.
         if all([include in sequence for include in words]):
             i = sequence.index(words[0])
@@ -74,6 +81,27 @@ def matches(sequence, words, match):
                     i = None
 
         return False
+    elif match == "relative":
+        # To match a relative, certainly all the words must be included.
+        if all([include in sequence for include in words]):
+            i = sequence.index(words[0])
+
+            while i is not None:
+                try:
+                    sequence.index(words[1], i + 1)
+                    return True
+                except ValueError as e:
+                    pass
+
+                try:
+                    i = sequence.index(words[0], i + 1)
+                except ValueError as e:
+                    i = None
+
+        return False
+
+    raise ValueError("unknown match: %s" % match)
+
 
 
 if __name__ == "__main__":
