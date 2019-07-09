4c070b970bff15f4933547a4718cd213d953da49
Lindsey Sawatzky <sawatzky.lindsey@gmail.com>
2019-07-08 22:24:49 -0700

    Allow for arbitrary queries through command invocation.

--- pattern-query.py
+++ pattern-query.py
@@ -20,6 +20,7 @@ def main():
     ap.add_argument("--db-kind", choices=["postgres", "sqlite"])
     ap.add_argument("data_dir")
     ap.add_argument("sequential_dir")
+    ap.add_argument("predicate", nargs="+")
     aargs = ap.parse_args(sys.argv[1:])
     setup_logging(".%s.log" % os.path.splitext(os.path.basename(__file__))[0], aargs.verbose, False, True, True)
     logging.debug(aargs)
@@ -27,14 +28,11 @@ def main():
     lstm = sequential.load_model(aargs.data_dir, aargs.sequential_dir)
     query_engine = domain.QueryEngine(lstm, "moot", "postgres")
 
-    # Hard coded - fiddel with manually
-    predicates = Predicates([
-        {"cells-0": {32: (0.5, None)}},
-        {"cells-0": {32: (0.5, "lt")}}
-    ])
+    predicates = Predicates(predicate_strs=aargs.predicate)
+    logging.debug("invoking query: %s" % (predicates.as_strs()))
     result = query_engine.find(0.1, predicates)
     dump = json.dumps(result.as_json())
-    logging.debug("%s\n  %s" % (predicates.as_strs(), dump))
+    logging.debug("result: %s" % (dump))
     print(dump)
     return 0
 
